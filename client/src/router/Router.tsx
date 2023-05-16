7d1d0ec73aec2371ec2d75302a439f5ba56f620c
pablo perez gonzalez <pabloperezgon91@gmail.com>
2023-05-16 09:05:33 +0200

    background and navbar

--- client/src/router/Router.tsx
+++ client/src/router/Router.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import HomePageView from '../app/view/home/HomePageView'
 
 function Router() {
+  const router = createBrowserRouter([
+    {
+    path: "/",
+    element:<HomePageView />
+  }
+])
   return (
     <div>
-        
+        <RouterProvider router={router} />
     </div>
   )
 }
